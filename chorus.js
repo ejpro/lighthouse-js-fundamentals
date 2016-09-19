4de8eccca15d0a017b70757767aa8149de602dda
Emily Prowse <ejprowse@gmail.com>
2016-09-19 01:02:27 +0000

    Added Module 4: Looping Exercises

---
+++ chorus.js
@@ -0,0 +1,11 @@
+var chorus = "Let's Dance!";
+var repeat = 0;
+while (repeat < 10) {
+	if (repeat === 5) {
+	console.log("*change key*");
+	}
+
+	console.log(chorus);
+	repeat++;
+}
+console.log("Until the sun comes up!");
